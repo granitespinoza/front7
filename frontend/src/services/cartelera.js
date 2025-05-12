4e1a458bc8b095c4a8693e525ca15b5c89395fa0
granitespinoza <granit.espinoza@utec.edu.pe>
2025-05-11 21:04:40 -0500

    wewewe

--- frontend/src/services/cartelera.js
+++ frontend/src/services/cartelera.js
@@ -1,7 +1,3 @@
 import api from './api';
-
-export const listarPeliculas = () =>
-  api.get('/cartelera');
-
-export const obtenerPelicula = id =>
-  api.get(`/cartelera/${id}`);
+export const listarPeliculas = () => api.get('/cartelera');
+export const obtenerPelicula   = id => api.get(`/cartelera/${id}`);
